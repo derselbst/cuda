b94011fa7d1aef9f3ffeb50cdbe1b75f53fdff8f
Tom M <tom.mbrt@googlemail.com>
2017-06-01 10:26:44 +0200

    Update calc.cu

--- calc.cu
+++ calc.cu
@@ -1,21 +1,37 @@
 
 #include "types.h"
 
+#define ACCESS(ARRAY, SET_IDX, LDA, ELEMENT) ARRAY[SET_IDX + ELEMENT*lda]
 
-bool fitPoints(const point_t* pts, const my_size_t set_idx, const my_size_t lda, real_t& slope_out, real_t& y_out)
+__global__ void kernel()
 {
-#define ACCESS(ELEMENT) pts[set_idx + ELEMENT*lda]
 
-    const my_size_t nPoints = ACCESS(0).n;
+    const my_size_t nPoints = ACCESS(pts, myAddr, nCols, 0).n;
+
+    const float x = ACCESS(pts, myAddr, nCols, 1).z;
+    const float y = ACCESS(pts, myAddr, nCols, 1).force;
+    
+    // get contact idx and split idx
+    
+    // polyfit sample data (first part)
+    real_t slope;
+    real_t yIntersect;
+    fitPoints(&ACCESS(pts, 0, nCols, 2), contact_idx+1, // polyfit from 2 element (i.e. first data point) up to contact idx
+              myAddr, nCols, slope, yIntersect);
+}
+
+bool fitPoints(const point_t* pts, my_size_t nPoints, const my_size_t set_idx, const my_size_t lda, real_t& slope_out, real_t& y_out)
+{
     if(nPoints <= 1)
     {
         // Fail: infinitely many lines passing through this single point
         return false;
     }
+    
     real_t sumX=0, sumY=0, sumXY=0, sumXX=0;
     for(my_size_t i=0; i<nPoints; i++)
     {
-        point_t tmp = ACCESS(i);
+        point_t tmp = ACCESS(pts, set_idx, lda, i);
         sumX += tmp.z;
         sumY += tmp.force;
         sumXY += tmp.z * tmp.force;
@@ -33,8 +49,6 @@ bool fitPoints(const point_t* pts, const my_size_t set_idx, const my_size_t lda,
     slope_out = (sumXY - sumX * yMean) / denominator;
     y_out = yMean - slope_out * xMean;
     return true;
-
-#undef ACCESS
 }
 
 int main()
